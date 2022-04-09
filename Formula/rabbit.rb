7ea32382b11621ebeb553c4533710cede2026acf
ilpanda <litencent@gmail.com>
2022-04-10 01:03:15 +0800

    Update rabbit.rb

--- Formula/rabbit.rb
+++ Formula/rabbit.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Rabbit < Formula
   homepage "https://github.com/ilpanda/rabbit"
-  url "https://github.com/ilpanda/rabbbit/releases/download/1.0.0/rabbit.zip"
+  url "https://github.com/ilpanda/rabbit/releases/download/1.0.0/rabbit.zip"
   version "1.0.0"
   sha256 "7a5afeb8b62b7f05670111abf2497c2c7e34caf367b15aa87a052d6e450a8672"
 
