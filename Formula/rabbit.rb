84fcf5d47aacdcf065d745edfd03116d96c01c14
liwei <litencent@gmail.com>
2022-04-09 23:41:22 +0800

    init rabbit

---
+++ Formula/rabbit.rb
@@ -0,0 +1,24 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Rabbit < Formula
+  desc ""
+  homepage "https://github.com/ilpanda/rabbit"
+  url "rabbit"
+  version "1.0.0"
+  sha256 ""
+
+  depends_on "openjdk"
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin lib]
+    (bin/"rabbit").write_env_script libexec/"bin/rabbit",
+      :JAVA_HOME => "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+  end
+
+  test do
+    output = shell_output("#{bin}/rabbit--help")
+    assert_includes output, "Usage: rabbit"
+  end
+end
